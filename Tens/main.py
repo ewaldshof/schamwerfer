f2337c68923334f357f4108272a7988acd93370c
Thomas Lehmann <Tom@Thomass-MacBook-Pro-3.local>
2019-10-03 18:09:23 +0200

    Blinker

--- Tens/main.py
+++ Tens/main.py
@@ -1,4 +1,14 @@
-# This is your main script.
+from machine import UART, Pin
+import time
+PulsePin = 12
+PowerPin = 13
+pPulse=Pin(PulsePin,Pin.OUT)
+pHiLo=Pin(PowerPin,Pin.OUT)
 
 
 print("Hello, world1!")
+
+while True:
+    time.sleep(1)
+    pHiLo.value(1*(pHiLo.value() !=1))
+ 
