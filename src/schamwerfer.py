af5b007cb4f9459ae4cfe7f49f7df0fd3c93474b
Kolja Sulimma <kolja@sulimma.de>
2019-10-04 20:54:00 +0200

    Add Position compatible pointAt method

--- src/schamwerfer.py
+++ src/schamwerfer.py
@@ -14,11 +14,14 @@ class Schamwerfer:
     def update(self, dwm, id, pos):
         print(id, pos)
         if id == self.tag_id:
-            self.beam.pointAt(pos.x, pos.y, self.fixed_z if self.fixed_z is not None else pos.z)
-            self.beam.update()
+            self.pointAt(pos)
 
     def listen(self):
         self.dwm.listen(self.update)
+
+    def pointAt(self, pos):
+        self.beam.pointAt(pos.x, pos.y, self.fixed_z if self.fixed_z is not None else pos.z)
+        self.beam.update()
 
 
 # Example: Read from ttyS5 and locate the beam at a certain position in the room.
