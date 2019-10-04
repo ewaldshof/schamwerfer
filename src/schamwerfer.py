58166c5d1df8bb6eb074e23cdf9a05ec36117d6f
Tim Weber <scy@scy.name>
2019-10-04 17:34:11 +0200

    Set beam color and actually listen

--- src/schamwerfer.py
+++ src/schamwerfer.py
@@ -7,18 +7,19 @@ class Schamwerfer:
     def __init__(self, serial_port, tag_id, beam_pos, beam_pan, beam_tilt):
         self.tag_id = tag_id
         self.dwm = DWM1001(serial_port)
-        self.dmx = MiniBeam(1, beam_pos.x, beam_pos.y, beam_pos.z, beam_pan, beam_tilt)
+        self.beam = MiniBeam(1, beam_pos.x, beam_pos.y, beam_pos.z, beam_pan, beam_tilt)
+        self.beam.setRGBWDS(0, 0, 0, 255)
 
     def update(dwm, id, pos):
         print(id, pos)
         if id == self.tag_id:
-            self.dmx.pointAt(pos.x, pos.y, pos.z)
+            self.beam.pointAt(pos.x, pos.y, pos.z)
 
     def listen():
         self.dwm.listen(self.update)
 
 
-# Example: Read from ttyS5 and print positions to console.
+# Example: Read from ttyS5 and locate the beam at a certain position in the room.
 if __name__ == "__main__":
     Schamwerfer(
         "/dev/ttyS5",
@@ -26,5 +27,4 @@ if __name__ == "__main__":
         Position(6.56, 3.69, 1.08),
         0,
         0,
-    )
-    DWM1001("/dev/ttyS5").listen(printer)
+    ).listen()
