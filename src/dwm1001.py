eeb5ca2a4d44b7c113743984ae7b5f1e8885050a
Kolja Sulimma <kolja@sulimma.de>
2019-10-04 21:32:40 +0200

    Allow limiting to a minimum confidence

--- src/dwm1001.py
+++ src/dwm1001.py
@@ -5,12 +5,13 @@ import serial
 
 class DWM1001:
 
-    def __init__(self, port, refresh_hz=20):
+    def __init__(self, port, refresh_hz=20, min_confidence=0):
         self.serial = serial.Serial(port=port, baudrate=115200)
         self.no_prompt = re.compile("^dwm> ")
         self.listening = False
         self.min_delay = 1 / refresh_hz
         self.last_msg = None
+        self.min_confidence = min_confidence
 
     def listen(self, callback):
         self.send()
@@ -24,10 +25,11 @@ class DWM1001:
             line = self.no_prompt.sub("", line.strip()).split(",")
             now = time.perf_counter()
             if len(line) >= 8 and (self.last_msg is None or (now - self.last_msg > self.min_delay)):
-                type, id = line[0], line[2]
+                type, id, confidence = line[0], line[2], int(line[6])
                 pos = Position(*line[3:6])
-                self.last_msg = now
-                callback(self, id, pos)
+                if confidence >= self.min_confidence:
+                    self.last_msg = now
+                    callback(self, id, pos)
 
     def send(self, string=""):
         self.serial.write((string + "\r\n").encode("utf-8"))
