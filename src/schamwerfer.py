54a7ad385d278f4bc611eebef7a007774af97653
Tim Weber <scy@scy.name>
2019-10-04 17:13:21 +0200

    Untested prototype that connects the parts

--- src/schamwerfer.py
+++ src/schamwerfer.py
@@ -1,7 +1,30 @@
+from dmx import MiniBeam
 from dwm1001 import DWM1001
+from position import Position
+
+class Schamwerfer:
+
+    def __init__(self, serial_port, tag_id, beam_pos, beam_pan, beam_tilt):
+        self.tag_id = tag_id
+        self.dwm = DWM1001(serial_port)
+        self.dmx = MiniBeam(1, beam_pos.x, beam_pos.y, beam_pos.z, beam_pan, beam_tilt)
+
+    def update(dwm, id, pos):
+        print(id, pos)
+        if id == self.tag_id:
+            self.dmx.pointAt(pos.x, pos.y, pos.z)
+
+    def listen():
+        self.dwm.listen(self.update)
+
 
 # Example: Read from ttyS5 and print positions to console.
 if __name__ == "__main__":
-    def printer(dwm, id, pos):
-        print(id, pos)
+    Schamwerfer(
+        "/dev/ttyS5",
+        "0689",
+        Position(6.56, 3.69, 1.08),
+        0,
+        0,
+    )
     DWM1001("/dev/ttyS5").listen(printer)
