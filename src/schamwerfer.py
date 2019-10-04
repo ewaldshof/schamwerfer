9e8d4d1d77abf3a477865d90331fcd874bdf5cef
Kolja Sulimma <kolja@sulimma.de>
2019-10-04 20:51:48 +0200

    Allow setting a fixed Z

--- src/schamwerfer.py
+++ src/schamwerfer.py
@@ -4,8 +4,9 @@ from position import Position
 
 class Schamwerfer:
 
-    def __init__(self, serial_port, tag_id, beam_pos, beam_pan, beam_tilt):
+    def __init__(self, serial_port, tag_id, beam_pos, beam_pan, beam_tilt, fixed_z=None):
         self.tag_id = tag_id
+        self.fixed_z = fixed_z
         self.dwm = DWM1001(serial_port)
         self.beam = MiniBeam(1, beam_pos.x, beam_pos.y, beam_pos.z, beam_pan, beam_tilt)
         self.beam.setRGBWDS(0, 0, 0, 255)
@@ -13,7 +14,7 @@ class Schamwerfer:
     def update(self, dwm, id, pos):
         print(id, pos)
         if id == self.tag_id:
-            self.beam.pointAt(pos.x, pos.y, pos.z)
+            self.beam.pointAt(pos.x, pos.y, self.fixed_z if self.fixed_z is not None else pos.z)
             self.beam.update()
 
     def listen(self):
